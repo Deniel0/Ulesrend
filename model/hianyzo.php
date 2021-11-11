2ae626d4612cc9ace9f865c69dc64647740ce1fa
Deniel0 <sutod820@gmail.com>
2021-11-11 11:51:41 +0100

    Update hianyzo.php

--- model/hianyzo.php
+++ model/hianyzo.php
@@ -1,11 +1,15 @@
 <?php
 
   require_once 'kijeloltfelhasznalok.php';
-  
+
 class hianyzo extends kijeloltfelhasznalok{
 
   function __construct() {
     $this->tablaNev = 'hianyzok';
+  }
+  //remove_id elkészítése
+  public function remove_id(){
+      
   }
 }
 
