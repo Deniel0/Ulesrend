41eb1def8c7bed0a9213b8f2b662d23ceaa22cf7
Deniel0 <sutod820@gmail.com>
2021-12-02 10:07:58 +0100

    Double_up

--- upload.php
+++ upload.php
@@ -1,10 +1,18 @@
 <?php
-if(isset($_FILES["fileToUpload"])){
+if(isset($_FILES["fileToUpload1"])){
   $target_dir = "uploads/";
-  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $target_file = $target_dir . basename($_FILES["fileToUpload1"]["name"]);
 
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+if (move_uploaded_file($_FILES["fileToUpload1"]["tmp_name"], $target_file)) {
   echo "The file has been uploaded.";
+  }
+}
+if(isset($_FILES["fileToUpload2"])){
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload2"]["name"]);
+
+if (move_uploaded_file($_FILES["fileToUpload2"]["tmp_name"], $target_file)) {
+  echo "\nThe file has been uploaded.";
   }
 }
 ?>
@@ -12,8 +20,9 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 <body>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
+  Select image to upload: <br>
+  <input type="file" name="fileToUpload1" id="fileToUpload"><br>
+  <input type="file" name="fileToUpload2" id="fileToUpload"><br>
   <input type="submit" value="Upload Image" name="submit">
 </form>
 </body>
