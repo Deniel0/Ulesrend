fa2f45ab701538ac6d36a5bcb220eaf4d732a653
Deniel0 <sutod820@gmail.com>
2021-12-02 12:27:37 +0100

    Most már jó

--- upload.php
+++ upload.php
@@ -1,27 +1,31 @@
 <?php
+
+$i = 0;
+$errors = array();
+
 if(isset($_FILES["fileToUpload"])){
-  print_r($_FILES["fileToUpload"]);
+  //sprint_r($_FILES["fileToUpload"]);
   $target_dir = "uploads/";
-  $i = 0;
-  $errors = array();
+  
+  $allowed_filetypes=array('image/png', 'image/jpg', 'image/jpeg');
 
   foreach($_FILES["fileToUpload"]["name"] as $key => $name){
     $target_file = $target_dir . basename($name);
 
-    if ($_FILES["fileToUpload"]["size"][$key] > 1024000) {
-      $errors[0][]="A $name túl nagy, 100KB-nál nem lehet nagyobb.";
+    if ($_FILES["fileToUpload"]["size"][$key] > 102400) {
+      $errors[$key][]="\nA $name túl nagy, 100KB-nál nem lehet nagyobb.";
     }
-    if ($_FILES["fileToUpload"]["size"][$key] > 1024) {
-      $errors[0][]="A $name túl kicsi, 1KB-nál nem lehet kisebb.";
+    if ($_FILES["fileToUpload"]["size"][$key] < 1024) {
+      $errors[$key][]="\nA $name túl kicsi, 1KB-nál nem lehet kisebb.";
     }
     if (!in_array($_FILES["fileToUpload"]["type"][$key], $allowed_filetypes)) {
-      $errors[$key][]="A $name nem jpg vagy png.";
+      $errors[$key][]="\nA $name nem jpg vagy png.";
     }
-    if (!$errors[$key]){
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
-      $i++;
+    if (!isset($errors[$key])){
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
+        $i++;
+      }
     }
-  }
   }
 }
 ?>
@@ -39,7 +43,7 @@ if($errors){
 ?>
 <form action="upload.php" method="post" enctype="multipart/form-data">
   Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload" multiple>
+  <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
   <input type="submit" value="Upload" name="submit">
 </form>
 
