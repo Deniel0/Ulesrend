93406bad11ed23df2de2e947c6a625abb77ae033
Deniel0 <sutod820@gmail.com>
2021-12-02 12:04:42 +0100

    hibásv2.0

--- upload.php
+++ upload.php
@@ -8,19 +8,35 @@ if(isset($_FILES["fileToUpload"])){
   foreach($_FILES["fileToUpload"]["name"] as $key => $name){
     $target_file = $target_dir . basename($name);
 
-    if ($_FILES["fileToUpload"]["size"][$key] > 1002400) {
-      $errors[0][]="$name file is too large.";
-      $uploadOk = 0;
+    if ($_FILES["fileToUpload"]["size"][$key] > 1024000) {
+      $errors[0][]="A $name túl nagy, 100KB-nál nem lehet nagyobb.";
     }
+    if ($_FILES["fileToUpload"]["size"][$key] > 1024) {
+      $errors[0][]="A $name túl kicsi, 1KB-nál nem lehet kisebb.";
+    }
+    if (!in_array($_FILES["fileToUpload"]["type"][$key], $allowed_filetypes)) {
+      $errors[$key][]="A $name nem jpg vagy png.";
+    }
+    if (!$errors[$key]){
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
       $i++;
     }
+  }
   }
 }
 ?>
 
 <body>
-
+<?php
+if($i > 0)echo "$i fájl feltöltve";
+if($errors){
+  foreach($errors as $error){
+    foreach($error as $errorMsg){
+      echo "$errorMsg";
+    }
+  }
+}
+?>
 <form action="upload.php" method="post" enctype="multipart/form-data">
   Select image to upload:
   <input type="file" name="fileToUpload" id="fileToUpload" multiple>
