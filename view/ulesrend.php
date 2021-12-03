7f0096f0c4798a55a19dfb97c56834898ec2570b
Deniel0 <sutod820@gmail.com>
2021-12-03 12:03:42 +0100

    ja

--- view/ulesrend.php
+++ view/ulesrend.php
@@ -2,19 +2,12 @@
 <tr>
 	<th colspan="3">
 		<h2>Ülésrend</h2>
-		<?php
-		if(isset($_POST['submit'])){ 
-			$filepath = "uploads/" . $_FILES["file"]["name"];
-		if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath));
-		}
-		?>
 	</th>
-
 	<th colspan="3">
 	<?php
 	if(!empty($_SESSION["id"])) {
 		if(in_array($_SESSION["id"], $adminok)) {
-		?>
+	?>
 		<form action="index.php?page=ulesrend" method="post">
 		Hiányzó: 	<select name="hianyzo_id">
 			<?php
@@ -27,19 +20,30 @@
 			?>
 					</select><br>
 		<input type="submit">
-		</form>						
-		<?php 
+		</form>
+
+	<?php 
 		}
 	}
 	?>
 	</th>
 </tr>
+
+<!--Alsó rész-->
+<!--Image upload-->
 	<form action="index.php?page=ulesrend" method="post" enctype="multipart/form-data">
-		<br>Select image to upload:<br><br>
-  		<input type="file" name="file"><br><br>
+		<br>Select image to upload to:
+  		<input type="file" name="file">
   		<input type="submit" value="Upload" name="submit">
 	</form>
+<!--Emberek-->
 	<?php
+	//Image upload php
+	if(isset($_POST['submit'])){ 
+		$filepath = "uploads/" . $_FILES["file"]["name"];
+	if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath));
+	}
+	//Image upload php vége
 	if ($tanuloIdk) {
 		$sor = 0;
 		foreach($tanuloIdk as $row) {
@@ -57,9 +61,10 @@
 				if($row == $tanar) $plusz .=  ' colspan="2"';
 					echo "<td".$plusz.">" . $tanulo->get_nev();
 				if(!empty($_SESSION["id"])) {
+					echo "<img src=".$filepath." height=100 width=150 />"; //upload
 					if(in_array($_SESSION["id"], $adminok)) {
-						echo "<img src=".$filepath." height=100 width=150 />";
 						if(in_array($row, $hianyzok)) echo '<br><a href="index.php?page=ulesrend&nem_hianyzo='.$row.'">Nem hiányzó</a>';
+						
 					}
 				}
 				echo "</td>";
@@ -70,16 +75,7 @@
 		echo "0 results";
 	}
 	$conn->close();
-	//include 'upload_2.php';
 	?>
 </table>
-	<?php
-		if(isset($_POST['submit'])){ 
-			$filepath = "uploads/" . $_FILES["file"]["name"];
-		if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) {
-			echo "<img src=".$filepath." height=200 width=300 />";
-		}
-		}
-?>
 </body>
 </html>
