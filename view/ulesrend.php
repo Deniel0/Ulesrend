102c2f03a70f6217bb4f94ea7b420bcde1ce509f
Deniel0 <92303546+Deniel0@users.noreply.github.com>
2021-12-03 14:09:27 +0100

    backup
    Visszaállítva az utolsó működő verzióra.
    Sql nem működik tesztelés nem lehtséges

--- view/ulesrend.php
+++ view/ulesrend.php
@@ -32,20 +32,18 @@
 <!--Alsó rész-->
 <!--Image upload-->
 	<form action="index.php?page=ulesrend" method="post" enctype="multipart/form-data">
-		<br>Select image to upload to:<select name="profilkepid">
-		<?php
-		if ($tanuloIdk) {
-				foreach($tanuloIdk as $row) {
-					$tanulo->set_user($row, $conn);
-					if($tanulo->get_nev()) echo '<option value="'.$row.'">'.$tanulo->get_nev().'</option>';
-				}
-			}
-			?>
+		<br>Select image to upload to:
   		<input type="file" name="file">
   		<input type="submit" value="Upload" name="submit">
 	</form>
 <!--Emberek-->
 	<?php
+	//Image upload php
+	if(isset($_POST['submit'])){ 
+		$filepath = "uploads/" . $_FILES["file"]["name"];
+	if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath));
+	}
+	//Image upload php vége
 	if ($tanuloIdk) {
 		$sor = 0;
 		foreach($tanuloIdk as $row) {
@@ -61,10 +59,9 @@
 				if(in_array($row, $hianyzok)) $plusz .=  ' class="missing"';
 				if($row == $en)$plusz .=  ' id="me"';
 				if($row == $tanar) $plusz .=  ' colspan="2"';
-				$plusz .=" style=background-image: url(uploads/$row.jpg)";
 					echo "<td".$plusz.">" . $tanulo->get_nev();
 				if(!empty($_SESSION["id"])) {
-					//echo "<img src=".$filepath." height=100 width=150 />";
+					echo "<img src=".$filepath." height=100 width=150 />"; //upload
 					if(in_array($_SESSION["id"], $adminok)) {
 						if(in_array($row, $hianyzok)) echo '<br><a href="index.php?page=ulesrend&nem_hianyzo='.$row.'">Nem hiányzó</a>';
 						
